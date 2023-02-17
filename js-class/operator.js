6b27d9fdac09ecfe54bfc5fd415fa71b6a52b1aa
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-02-17 09:48:39 +0100

    increment added

--- js-class/operator.js
+++ js-class/operator.js
@@ -1,16 +1,44 @@
 // what are operands are variablename that are worked on
-let x = 2
-let y = 3
+let x = 6
+let y = 4
  //x and y are the operands
 
 
 //Operator
 //addition operator
  let result1 = x + y 
-console.log("Addition is", result1)
+console.log("Addition is " + result1)
 
 //subtraction
 let result2 = x - y
-console.log("Subtraction", result2)
+console.log("Subtraction " + result2)
+
+//multiplication
+let result3 = x * y
+console.log("multiplication is: " + result3)
+
+// modules
+let result4 = x % y
+console.log("modules is " + result4)
+
+// division
+let result5 = x / y
+console.log("division: "+ result5)
+
+//exponential
+let result6 = x ** y
+console.log("exponential" + result6)
+
+
+//increament ++
+//preincrement
+let a = 10;
+let result7 = 2 + ++a
+console.log("PreIncrement", result7)
+//Post increment: any increment that happen to the valueName is not considered.
+let b = 10;
+let result8 = 2 + b++
+console.log("postIncrement", result8)
 
 //
+
