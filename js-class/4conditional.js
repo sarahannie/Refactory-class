1fbf1ef8b80e6f767b65748588597e1d8b171e62
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-02-20 11:09:09 +0100

    looping-for

--- js-class/4conditional.js
+++ js-class/4conditional.js
@@ -17,3 +17,36 @@ switch (startRating) {
         console.log("enter a valid number")
         break;
 }
+
+switch( new Date().getDate()){
+    case 0:
+        console.log("Sunday")
+        break;
+    case 20:
+        console.log("Monday")
+        break;
+    case 21:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("Wednesday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    default:
+        console.log("no date of the week")
+}
+
+//looping
+//for loop
+/*
+for(initialization; condition; incrementing){
+    //loop body
+} */
+
+let i;
+
+for(i = 1; i <= 40; i++){
+    console.log('value of i is:' +i)
+}
