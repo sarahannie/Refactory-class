82fb2c72c06ad386fed3de454756076fcbfef84b
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-02-23 09:53:41 +0100

    replacing array

--- js-class/array.js
+++ js-class/array.js
@@ -1,3 +1,10 @@
 let number = ["Volleyball","basket","Football", 1,2 ,3, true, false, null, undefined]
 //array is a heterogenous meaning it can contain any data type
 console.log(number[1])
+
+let random =["Rome", "uganda","southAfrica","Nigeria",["Ghana", "Cameroon"]]
+console.log(random[4][1])
+
+//Replacing in the array
+random[0] = "France"
+console.log(random)
