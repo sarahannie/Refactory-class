4d1b6d83815607a981e514fa5b15481a8bd84450
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-02-24 09:48:16 +0100

    flatting array

--- js-class/nestedarray.js
+++ js-class/nestedarray.js
@@ -9,4 +9,25 @@ hobbies[1].push("she like singing pop song")
 
 console.log(hobbies)
 
-//
+//splice using to add
+hobbies.splice(0,1, ["dancing also"])
+
+console.log(hobbies)
+
+
+// using foreach in array
+
+
+hobbies = [["Swimming",4], ["Singing", 5], ["netball", 8], ['football',6]]
+hobbies.forEach((hobby) =>{
+    hobby.pop(1);
+})
+console.log("After delete second item from each nested array", hobbies)
+
+
+/// flating array
+hobbies = [["Swimming",4], ["Singing", 5], ["netball", 8], ['football',6]]
+
+const flatarray = hobbies.flat();
+
+console.log(flatarray)
