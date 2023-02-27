5b56d857a6171526b95bb46e7c3cde2f8b9049cd
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-02-27 10:15:16 +0100

    object in js

---
+++ js-class/object.js
@@ -0,0 +1,22 @@
+// object are collection of key value pairs
+// object;s key are sting or symbols
+//key: values
+
+let person = {
+    name : 'Annie',
+    Age: 24,
+    hobbies: ['Swimming', 'Gisting','Reading','Coding']
+};
+
+//Accessing Properties
+person.name // dot notation
+
+person['Age'] //bracket notation
+
+//Adding new properties 
+person.height = '5ft'
+
+person['gender'] = 'Female'
+
+console.log(person)
+
