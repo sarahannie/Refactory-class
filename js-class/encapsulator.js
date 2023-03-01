c75b4c5a007a2f0ca30c006babfe185431e3e941
sarahannie <87903267+sarahannie@users.noreply.github.com>
2023-03-01 10:10:56 +0100

    encapsulation

---
+++ js-class/encapsulator.js
@@ -0,0 +1,31 @@
+class Worker{
+    constructor(){
+        let name;
+        let rating;
+    }
+
+    //getter method
+    getName(){
+        return this.name
+    }
+    //setter method
+    setName(name){
+        this.name = name
+    }
+    //getter method
+    getRating(){
+        return this.rating;
+    }
+
+    //setter method 
+    setRating(rating){
+        this.rating = rating
+    }
+}
+
+//create instances of class worker
+let worker = new Worker();
+worker.setName("Sarah")
+worker.setRating("10/10")
+
+console.log(worker.setName())
